4ebdbb678fae8155ad7b305da5296916c7d221ef
cangelosilima <cangelosilima@gmail.com>
2024-03-16 20:47:18 -0300

    ci: change observability config

--- azure/main.tf
+++ azure/main.tf
@@ -44,17 +44,6 @@ resource "azurerm_service_plan" "auth_plan" {
   }
 }
 
-resource "azurerm_application_insights" "auth_app_insights" {
-  name                = "auth-app-insights"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  application_type    = "other"
-
-  tags = {
-    environment = azurerm_resource_group.resource_group.tags["environment"]
-  }
-}
-
 data "azurerm_storage_account" "storage_account_terraform" {
   name                = "sandubaterraform"
   resource_group_name = data.azurerm_resource_group.main_group.name
@@ -66,7 +55,7 @@ data "azurerm_virtual_network" "virtual_network" {
 }
 
 data "azurerm_subnet" "api_subnet" {
-  name                 = "fiap-tech-challenge-api-subnet"
+  name                 = "fiap-tech-challenge-auth-subnet"
   virtual_network_name = data.azurerm_virtual_network.virtual_network.name
   resource_group_name  = data.azurerm_virtual_network.virtual_network.resource_group_name
 }
@@ -88,9 +77,7 @@ resource "azurerm_linux_function_app" "linux_function" {
   }
 
   site_config {
-    application_insights_key               = azurerm_application_insights.auth_app_insights.instrumentation_key
-    application_insights_connection_string = azurerm_application_insights.auth_app_insights.connection_string
-    always_on                              = true
+    always_on = true
     application_stack {
       docker {
         registry_url = "https://index.docker.io"
@@ -104,5 +91,31 @@ resource "azurerm_linux_function_app" "linux_function" {
 
   tags = {
     environment = azurerm_resource_group.resource_group.tags["environment"]
+  }
+}
+
+data "azurerm_storage_account" "log_storage_account" {
+  name                = "sandubalog"
+  resource_group_name = "fiap-tech-challenge-observability-group"
+}
+
+data "azurerm_log_analytics_workspace" "log_workspace" {
+  name                = "fiap-tech-challenge-observability-workspace"
+  resource_group_name = "fiap-tech-challenge-observability-group"
+}
+
+
+resource "azurerm_monitor_diagnostic_setting" "function_monitor" {
+  name                       = "fiap-tech-challenge-auth-monitor"
+  target_resource_id         = azurerm_linux_function_app.linux_function.id
+  storage_account_id         = data.azurerm_storage_account.log_storage_account.id
+  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.log_workspace.id
+
+  enabled_log {
+    category = "FunctionAppLogs"
+  }
+
+  metric {
+    category = "AllMetrics"
   }
 }
